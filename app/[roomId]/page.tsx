3f1989ec53d6797bb25ad4a9b92cb7da5c6d72f8
mohitarora249 <mohitarora249@gmail.com>
2024-03-31 13:41:48 +0530

    redirect if invalid room id is passed

--- app/[roomId]/page.tsx
+++ app/[roomId]/page.tsx
@@ -1,17 +1,27 @@
 "use client";
 
+import { ROOM_ID_LENGTH } from "@/constants";
 import useMediaStream from "@/hooks/user-media-stream";
-import { useParams } from "next/navigation";
+import { redirect, useParams } from "next/navigation";
+import { useEffect } from "react";
 import ReactPlayer from "react-player";
 
 const Page = () => {
   const params = useParams();
   const { myStream } = useMediaStream();
+
   const roomId = params.roomId as string;
+
+  useEffect(() => {
+    if (roomId && roomId.length !== ROOM_ID_LENGTH) {
+      redirect("/?error=invalid-room-id");
+    }
+  }, []);
+
   console.log("roomId : ", roomId);
 
   return (
-    <div className="flex min-h-screen flex-col relative">
+    <div className="flex h-screen flex-col relative">
       {/* remote video stream */}
       {myStream && (
         <ReactPlayer
@@ -25,7 +35,7 @@ const Page = () => {
       )}
       {/* own video stream */}
       {myStream && (
-        <div className="h-56 w-64 absolute bottom-4 right-4 rounded-md">
+        <div className="h-56 w-64 absolute bottom-4 right-4">
           <ReactPlayer
             playing={true}
             height="100%"
