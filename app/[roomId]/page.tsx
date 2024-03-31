a281893a0d1dabab145c1a979692124e35cd93de
mohitarora249 <mohitarora249@gmail.com>
2024-03-31 13:13:37 +0530

    added own video stream on room page

--- app/[roomId]/page.tsx
+++ app/[roomId]/page.tsx
@@ -11,17 +11,29 @@ const Page = () => {
   console.log("roomId : ", roomId);
 
   return (
-    <div className="flex min-h-screen flex-col">
-      {/* remote video */}
+    <div className="flex min-h-screen flex-col relative">
+      {/* remote video stream */}
       {myStream && (
         <ReactPlayer
           playing={true}
           height="100vh"
           width="100vw"
           url={myStream}
-          // muted
+          muted
           // controls={true}
         />
+      )}
+      {/* own video stream */}
+      {myStream && (
+        <div className="h-56 w-64 absolute bottom-4 right-4 rounded-md">
+          <ReactPlayer
+            playing={true}
+            height="100%"
+            width="100%"
+            url={myStream}
+            muted
+          />
+        </div>
       )}
     </div>
   );
